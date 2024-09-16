af3bef4ac4b3bd65e9288317d6fe508b611cbe64
Jon Ellwood <jon.ellwood@berkeleycountysc.gov>
2024-09-16 15:35:38 -0400

    alpha website status checker component

---
+++ data/siteListData.js
@@ -0,0 +1,25 @@
+function siteListItems() {
+  const siteListItems = [
+    {
+      href: "https://berkeleycountysc.gov/",
+      //   icon: "employee",
+      text: "Berkeley County Website",
+    },
+    {
+      href: "https://sheriff.berkeleycountysc.gov/",
+      //   icon: "time-clock",
+      text: "Berkeley County Sheriff",
+    },
+    {
+      href: "https://agendas.berkeleycountysc.gov/meeting_list.php",
+      //   icon: "tshirt",
+      text: "Agendas",
+    },
+    {
+      href: "https://gis.berkeleycountysc.gov/",
+      //   icon: "jobs",
+      text: "GIS",
+    },
+  ];
+  return siteListItems;
+}
--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 // Created: 2024/09/12 13:12:49
-// Last modified: 2024/09/13 10:20:08
-include "./components/header.php" ?>
+// Last modified: 2024/09/16 15:10:19
+include "./components/header.php"
+?>
+<script src="./functions/checkURLOnline.js"></script>
+
 
 <body class="mode-dark theme-base">
     <div class="main">
         <?php include "./components/sidenav.php" ?>
         <div class="content">
             <div class="dash-main bg-dark">
+                <div class="cards-container">
+                    <div id="websiteStatus">
+                        <h4 class="component-header">Website Status Indicators</h4>
+                        <div id="urlStatus"></div>
+                    </div>
 
-                <h1>My Dashboard</h1>
 
+                </div>
             </div>
         </div>
     </div>
@@ -19,6 +27,9 @@ include "./components/header.php" ?>
 </body>
 
 </html>
+<script>
+    makeWebsiteStatusCards()
+</script>
 
 
 <style>
@@ -26,5 +37,28 @@ include "./components/header.php" ?>
         padding: 1rem !important;
         height: 100%;
         display: flex;
+    }
+
+    .websiteStatus {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    .cards-container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 20px;
+        min-height: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        /* background-color: light-dark(#5b89a1, #1b283c); */
+    }
+
+    .card-title a {
+        font-size: large !important;
     }
 </style>
